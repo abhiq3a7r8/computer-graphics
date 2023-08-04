5305c0101db29610a074a5d6c5d8c8cda173715a
abhiq3a7r8 <140839210+abhiq3a7r8@users.noreply.github.com>
2023-08-04 23:05:28 +0530

    Add files via upload

---
+++ DDA_algorithm.py
@@ -0,0 +1,28 @@
+x1 = float(input("Enter the starting coordinate of X: "))
+y1 = float(input("Enter the starting coordinate of Y: "))
+x2 = float(input("Enter the ending coordinate of X: "))
+y2 = float(input("Enter the ending coordinate of Y: "))
+
+dx = x2 - x1
+dy = y2 - y1
+
+m = dy / dx
+x = x1
+y = y1
+
+print("\n The Points are:")
+
+if m <= 1:
+    while round(x, 2) != x2 and round(y, 2) != y2:
+        x = x + 1
+        y = y + m
+        rx = round(x, 2)
+        ry = round(y, 2)
+        print(f"x = {rx:.2f}, y = {ry:.2f}")
+else:
+    while round(x, 2) != x2 and round(y, 2) != y2:
+        y = y + 1
+        x = x + 1 / m
+        rx = round(x, 2)
+        ry = round(y, 2)
+        print(f"x = {rx:.2f}, y = {ry:.2f}")
